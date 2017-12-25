ffc6345a8bb441356d7fb3c60ea401fe868cef25
Osman Zupcic <osmanzupcic@gmail.com>
2017-12-25 22:39:44 +0100

    check out old version

--- server.js
+++ server.js
@@ -39,22 +39,6 @@ var models = require('./models');
 
 app.set('port', process.env.PORT || 5000);
 
-models.sequelize.query('GRANT ALL ON heroku_ec0bfe9b1ae1583.* TO \'b9aa5643e9bf36\'@\'ip-10-76-156-41.eu-west-1.compute.internal\'', function(err) {
-	if(err){
-		console.log('An error appeared.');
-	} else{
-	   console.log('Error granting');
-	}   
-  });
-  
-models.sequelize.query('INSERT INTO admin (username,password) VALUES(\'admin\',\'1234\')', function(err) {
-	if(err){
-		console.log('An error appeared.');
-	} else{
-	   console.log('Admin added');
-	}   
-  });
-
 models.sequelize.sync().then(function () {
   var server = app.listen(app.get('port'), function() {
     console.log('Express server listening on port ' + server.address().port);
